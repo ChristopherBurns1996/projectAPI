9832677e96d90ebbd5bb6b175f68a0640c08b13a
Chris <Christopher.Burns@academytrainee.com>
2019-09-09 14:58:08 +0100

    committing app class

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,13 +1,14 @@
 package com.qa;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//this apparently brings everything with this annotation into scope/context
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
