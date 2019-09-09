cb7cb6f7d1343693556acaf7cdb254c64b776c53
Chris <Christopher.Burns@academytrainee.com>
2019-09-09 17:53:12 +0100

    Committing publisher repo

---
+++ src/main/java/com/qa/repository/PublishersRepository.java
@@ -0,0 +1,7 @@
+package com.qa.repository;
+
+import com.qa.models.Publishers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublishersRepository extends JpaRepository<Publishers, Long> {
+}
