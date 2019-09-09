6bc7c898e2cae7423edbeee11e39ede743b99741
Chris <Christopher.Burns@academytrainee.com>
2019-09-09 17:51:51 +0100

    Committing publisher model class

---
+++ src/main/java/com/qa/models/Publishers.java
@@ -0,0 +1,56 @@
+package com.qa.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Publishers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long publisherid;
+    String publisherName;
+    String currentCEO;
+    //Date yearEstablished;
+
+    public Publishers() {
+    }
+
+    public Publishers(String publisherName, String currentCEO) {
+        this.publisherName = publisherName;
+        this.currentCEO = currentCEO;
+    }
+
+    public Long getPublisherid() {
+        return publisherid;
+    }
+
+    public void setPublisherid(Long publisherid) {
+        this.publisherid = publisherid;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public String getCurrentCEO() {
+        return currentCEO;
+    }
+
+    public void setCurrentCEO(String currentCEO) {
+        this.currentCEO = currentCEO;
+    }
+
+    public void setPublisher(Publishers publisher){
+        this.publisherName = publisherName;
+        this.currentCEO = currentCEO;
+    }
+}
