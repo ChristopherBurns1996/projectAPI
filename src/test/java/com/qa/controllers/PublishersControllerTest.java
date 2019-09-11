817a82e9405f08185ed4070e9e8ffe2e496c0881
Chris <Christopher.Burns@academytrainee.com>
2019-09-11 14:00:51 +0100

    added publisher controller test class added a test to publisher controller test class

---
+++ src/test/java/com/qa/controllers/PublishersControllerTest.java
@@ -0,0 +1,52 @@
+package com.qa.controllers;
+
+import com.qa.models.Publishers;
+import com.qa.repository.PublishersRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PublishersControllerTest {
+
+    @InjectMocks
+    private PublishersController publishersController;
+
+    @Mock
+    private PublishersRepository repository;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void listAllPublishersTest(){
+        List<Publishers> publishersList = new ArrayList<>();
+        Publishers publisher = new Publishers();
+        publisher.setPublisherid(0l);
+        publisher.setPublisherName("Bungie");
+        publisher.setCurrentCEO("IDK");
+        publisher.setYearEstablished(2000);
+        publishersList.add(publisher);
+
+        when(repository.findAll()).thenReturn(publishersList);
+
+        assertEquals(publishersController.listAllPublishers().get(0).getPublisherName(), "Bungie");
+    }
+
+}
