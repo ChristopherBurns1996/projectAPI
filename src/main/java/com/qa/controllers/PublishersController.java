b205938b5e0e1c5c5a854e25b23c63d6bd4413ec
ChristopherBurns1996 <Christopher.Burns@academytrainee.com>
2019-09-26 16:57:41 +0100

    updating publisher controller class with comments

--- src/main/java/com/qa/controllers/PublishersController.java
+++ src/main/java/com/qa/controllers/PublishersController.java
@@ -15,16 +15,19 @@ public class PublishersController {
     @Autowired
     private PublishersRepository repository;
 
+    //Finds all of the publishers and returns them
     @RequestMapping(value = "publishers", method = RequestMethod.GET)
     public List<Publishers> listAllPublishers(){
         return repository.findAll();
     }
 
+    //Adds a publisher to the table
     @RequestMapping(value = "publishers", method = RequestMethod.POST)
     public Publishers addPublisher(@RequestBody Publishers publisher){
         return repository.saveAndFlush(publisher);
     }
 
+    //Updates a record in the table
     @Transactional
     @RequestMapping(value = "publishers/{publisherid}", method = RequestMethod.PUT)
     public Publishers updatePublisher(@RequestBody Publishers publisher, @PathVariable Long publisherid){
@@ -33,6 +36,7 @@ public class PublishersController {
         return existing;
     }
 
+    //Deletes a publisher from the table
     @RequestMapping(value = "publishers/{publisherid}", method = RequestMethod.DELETE)
     public Publishers deletePublisher(@PathVariable Long publisherid){
         Publishers existing = repository.findOne(publisherid);
